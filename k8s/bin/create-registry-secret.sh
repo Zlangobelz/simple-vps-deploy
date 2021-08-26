77e09ed496944a82c6925c97561696d465d2dc2f
Vlad Zinko <vlad.zinko@contractors.medallia.com>
2021-08-26 13:34:49 +0300

    added build on push to main

--- k8s/bin/create-registry-secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl create secret docker-registry ghcr-secret \
---docker-server=https://ghcr.io \
---docker-username=$USERNAME \
---docker-password=$PASSWORD
--- k8s/bin/helm-upgrade.sh
+++ k8s/bin/helm-upgrade.sh
@@ -3,9 +3,6 @@
 mv ./k8s/chart/Chart.yaml ./k8s/chart/Chart.old.yaml &&
   cat ./k8s/chart/Chart.old.yaml | grep -v appVersion > ./k8s/chart/Chart.yaml &&
 #  echo -e "\r\nappVersion: v${GITHUB_REF##*/}\r\n" >> ./.helm/app/Chart.yaml &&
-  echo -e "\r\nappVersion: \"v2\"\r\n" >> ./k8s/chart/Chart.yaml &&
-  cat ./k8s/chart/Chart.yaml
+  echo -e "appVersion: \"${APP_VERSION}\"" >> ./k8s/chart/Chart.yaml
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-cd ./k8s/chart && helm dependency build
+cd ./k8s/chart && helm upgrade "${APP_NAME}" .
