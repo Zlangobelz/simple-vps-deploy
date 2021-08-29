add739319ca3e1f48afad59d60ccb370906b38e7
Vlad Zinko <vlad.zinko@contractors.medallia.com>
2021-08-29 13:26:01 +0300

    fixed sh

--- k8s/bin/helm-upgrade.sh
+++ k8s/bin/helm-upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv ./k8s/chart/Chart.yaml ./k8s/chart/Chart.old.yaml &&
-  cat ./k8s/chart/Chart.old.yaml | grep -v appVersion > ./k8s/chart/Chart.yaml &&
+mv ./k8s/app/Chart.yaml ./k8s/app/Chart.old.yaml &&
+  cat ./k8s/app/Chart.old.yaml | grep -v appVersion > ./k8s/app/Chart.yaml &&
 #  echo -e "\r\nappVersion: v${GITHUB_REF##*/}\r\n" >> ./.helm/app/Chart.yaml &&
-  echo -e "appVersion: \"${APP_VERSION}\"" >> ./k8s/chart/Chart.yaml
+  echo -e "appVersion: \"${APP_VERSION}\"" >> ./k8s/app/Chart.yaml
